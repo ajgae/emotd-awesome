0af26e2a1be6c50b474d6720cf29d73f05619613
categorille <categorille@protonmail.com>
2021-02-19 00:53:37 +0100

    renamed stuff

--- emotd.lua
+++ emotd.lua
@@ -1,16 +1,9 @@
 ------------------------------------------------------------------
--- Feeling Word Of The Day for Awesome Window Manager
+-- `emotd` - A SIMPLE `awesome` WIDGET THAT HELPS YOU DEVELOP
+-- YOUR EMOTIONAL LITERACY
 --
--- Scroll through random feeling words until you find one that
--- describes your mood :)
---
--- A word is a single line retrieved from a text file, which is
--- by default the directory where this lua file is found.
--- Might have to adjust that manually (look for the 
--- WORD_FILE_DEFAULT variable).
---
--- In the word file, empty lines and lines whose first
--- non-whitespace character is a '#' will be skipped.
+-- Whenever you feel like it, scroll through random feeling
+-- words until you find one that describes your current mood.
 --
 -- @author: categorille @ protonmail dot com
 -- @copyleft 2021 categorille
@@ -19,11 +12,12 @@
 -- awesome widgets API
 local wibox = require("wibox")
 
-local HOME = os.getenv("HOME")
-local WIDGET_DIR = HOME .. "/.config/awesome/my-widgets/"
-local WORD_FILE_DEFAULT = WIDGET_DIR .. "words.txt"
+local HOME_DIR = os.getenv("HOME") .. "/"
+local WORDS_FILE_DEFAULT = HOME_DIR .. ".emotd_words"
 
 ------------UTILITY FUNCTIONS------------
+-- increments an array index modulo `mod` with
+-- indexing starting at 1
 local function increment(index, mod)
     local result = (index + 1) % (mod + 1)
     if result == 0 then
@@ -32,9 +26,11 @@ local function increment(index, mod)
     return result
 end
 
+-- decrements an array index modulo `mod` with
+-- indexing starting at 1
 local function decrement(index, mod)
     local result = index - 1
-    if result <= 0 then
+    if result < 1 then
         result = mod
     end
     return result
@@ -42,25 +38,25 @@ end
 
 
 -----------WORKER FUNCTION------------
-local wotd_widget = {}
+local emotd_widget = {}
 local function worker(user_args)
     -- Handle user arguments
     local args = user_args or {}
-    local word_file = args.word_file or WORD_FILE_DEFAULT
+    local word_file = args.words_file or WORDS_FILE_DEFAULT
     local prefix = args.prefix or ""
     local suffix = args.suffix or ""
     local hist_count = args.hist_count or 5
 
-    -- returns the text with pretties added
+    -- Returns the text as it should be displayed in the widget
     local function render_text(text)
         return prefix .. text .. suffix
     end
 
-    -- returns a random line from the word_file
+    -- Returns a random line from the word_file
     local function get_random_word()
         local word
         repeat -- Find a random non-comment, non-empty line
-            local f = io.popen("shuf " .. word_file, "r")
+            local f = assert(io.popen("shuf " .. word_file, "r"))
             word = f:read("*l")
         until not(word == "") and not(word:match("^[ \t]*#.*"))
         return word
@@ -68,7 +64,9 @@ local function worker(user_args)
 
     -- Keep track of history
     -- TODO find a better history system, currently moving
-    -- through an array mod hist_count
+    -- cycling an array modulo hist_count
+    -- FIXME current history system not working, first right
+    -- click does nothing
     local hist = {}
     local hist_index = 2
 
@@ -79,7 +77,7 @@ local function worker(user_args)
             hist[i] = 0
         end
         -- Build the widget
-        wotd_widget = wibox.widget {
+        emotd_widget = wibox.widget {
             text = render_text(init_word),
             align = "center",
             valign = "center",
@@ -87,7 +85,7 @@ local function worker(user_args)
         }
     end
 
-    wotd_widget:connect_signal("button::press", function(_, _, _, button)
+    emotd_widget:connect_signal("button::press", function(_, _, _, button)
         if (button == 1 or button == 3) then
             local result
             if button == 1 then -- New random word
@@ -101,14 +99,14 @@ local function worker(user_args)
                     result = hist[hist_index]
                 end
             end
-            wotd_widget.text = render_text(result)
+            emotd_widget.text = render_text(result)
         end
     end)
 
-    return wotd_widget
+    return emotd_widget
 end
 
-return setmetatable(wotd_widget, {
+return setmetatable(emotd_widget, {
     __call = function(_self, ...)
         return worker(...)
     end
